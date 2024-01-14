6d789358cc1e938789bf90b66f8c3f320397c4c0
LWJerri <loginmanx@gmail.com>
2024-01-14 16:48:09 +0100

    Display custom context menu if we have `__TAURI_METADATA__` property.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { onEventShowMenu } from "tauri-plugin-context-menu";
+import App from "./App";
 import "./index.css";
+
+if (window?.__TAURI_METADATA__) {
+  onEventShowMenu("contextmenu", async () => ({
+    items: [
+      {
+        label: "Reload window",
+
+        event() {
+          location.reload();
+        },
+      },
+    ],
+  }));
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
