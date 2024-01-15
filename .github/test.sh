df0bc66d68da4daa0a0e76b1b985635b41a9bb5a
LWJerri <loginmanx@gmail.com>
2024-01-15 21:04:39 +0100

    Remove sh step

--- .github/test.sh
+++
@@ -1,4 +0,0 @@
-echo "v$APP_VERSION, full changelog: $RELEASE_HTML_URL, link to download new files: $RELEASE_UPLOAD_URL" >> test.txt
-git checkout production
-git add . && git commit -am "Add test.txt"
-git push
