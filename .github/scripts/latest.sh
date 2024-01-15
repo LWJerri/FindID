ef30f80e1c18367f0aeeaf6936e72291016949f2
LWJerri <loginmanx@gmail.com>
2024-01-16 00:12:32 +0100

    1

--- .github/scripts/latest.sh
+++ .github/scripts/latest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo $RELEASE_ID
-
 response=$(curl -s https://api.github.com/repos/LWJerri/FindID/releases/$RELEASE_ID)
 
 latest_url=$(echo "$response" | jq -r '.assets | last(.[]).browser_download_url')
