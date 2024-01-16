80f30c754016d4d84930f809b93aab755652da56
LWJerri <loginmanx@gmail.com>
2024-01-16 12:37:52 +0100

    Add sh script to automatically update `latest.json` file for Tauri Updater

---
+++ .github/scripts/latest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+response=$(curl -s $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases/$RELEASE_ID)
+
+latest_url=$(echo "$response" | jq -r '.assets[] | select(.name == "latest.json").browser_download_url')
+
+curl -LJO $latest_url
+
+file_name=$(basename $latest_url)
+file_contents=$(cat $file_name)
+
+file_contents=$(echo "$file_contents" | sed 's/""/"With every new version FindID is getting better for you."/g')
+
+echo "$file_contents" > latest.json
